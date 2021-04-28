e1ef957f5efa2148fbf853188063b4becf03fb5c
Jose Manuel <jmdominguez@aytoalcobendas.org>
2021-04-28 12:44:22 +0200

    Actualizado

--- Caluculadora Simple/src/calculadora/CalculadoraSimple.java
+++ Caluculadora Simple/src/calculadora/CalculadoraSimple.java
@@ -1,7 +1,7 @@
 package calculadora;
 
-
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -22,110 +22,114 @@ public class CalculadoraSimple extends Application {
 	Label resLabel;
 	TextField xField;
 	TextField yField;
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		// layout para meter el valor de x
 		xField = new TextField("0");
-		HBox numxPanel = new HBox(new Label(" x = "),xField);
+		HBox numxPanel = new HBox(new Label(" x = "), xField);
 		// permite que se expanda horizontalmente
 		HBox.setHgrow(xField, Priority.ALWAYS);
-		
-		// layout para recoger el valor de y 
-		yField = new TextField("0");	
-		HBox numyPanel = new HBox(new Label(" y = "),yField);
-		HBox.setHgrow(yField, Priority.ALWAYS);
-		
-	
-		
-		// Cramos los botones para los botones
-		
-		Button sumaButton = new Button("+");
-		// Anchura por defecto (o preferida)
-		sumaButton.setPrefWidth(100);
-		// Anchura máxima infinita
-		sumaButton.setMaxWidth(Double.POSITIVE_INFINITY);
-		HBox.setHgrow(sumaButton, Priority.ALWAYS); 
-		sumaButton.setOnAction(e-> hazOperacion('+'));
-		
-		
-		Button restaButton = new Button("-");
-		restaButton.setPrefWidth(100);
-		restaButton.setMaxWidth(Double.POSITIVE_INFINITY);
-		HBox.setHgrow(restaButton, Priority.ALWAYS);
-		restaButton.setOnAction(e-> hazOperacion('-'));
 
-		Button prodtaButton = new Button("*");
-		prodtaButton.setPrefWidth(100);
-		prodtaButton.setMaxWidth(Double.POSITIVE_INFINITY);
-		HBox.setHgrow(prodtaButton, Priority.ALWAYS);
-		prodtaButton.setOnAction(e->hazOperacion('*'));
+		// layout para recoger el valor de y
+		yField = new TextField("0");
+		HBox numyPanel = new HBox(new Label(" y = "), yField);
+		HBox.setHgrow(yField, Priority.ALWAYS);
+
+		// Cramos los botones para los botones
+
+		Button sumaButton = creaBotonOp("+");
+
+		Button restaButton = creaBotonOp("-");
+
+		Button prodtaButton = creaBotonOp("*");
 		
-		Button divButton = new Button("/");
-		divButton.setPrefWidth(100);
-		divButton.setMaxWidth(Double.POSITIVE_INFINITY);
-		HBox.setHgrow(divButton, Priority.ALWAYS);
-		divButton.setOnAction(e-> hazOperacion('/'));
+		Button divButton = creaBotonOp("/");
 		
-		HBox botonesPanel = new HBox(sumaButton,restaButton,prodtaButton,divButton);
+
+		HBox botonesPanel = new HBox(sumaButton, restaButton, prodtaButton, divButton);
 		
+		// ** Si queremos que el contenedor de los  botones crezca a lo alto al agrandar la ventana
+		//botonesPanel.setMaxHeight(Double.POSITIVE_INFINITY);
+		//VBox.setVgrow(botonesPanel, Priority.ALWAYS);
+
 		// Etiqueta para mostrar el resultado
 		resLabel = new Label("x + y = 0");
 		resLabel.setAlignment(Pos.CENTER);
 		resLabel.setMaxWidth(Double.POSITIVE_INFINITY);
 		resLabel.setMaxHeight(Double.POSITIVE_INFINITY);
-        resLabel.setTextFill(Color.YELLOW);
-        resLabel.setFont(new Font(24.0));
-        resLabel.setStyle("-fx-font-weight:bold;-fx-background-color : #0000FF;");
-        
-        // Poner background con metodo setbackground es más tedioso.
-        //resLabel.setBackground(new Background(new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, Insets.EMPTY)));
-		
-        
-        //Permite expandirse horizontalmente
+		resLabel.setTextFill(Color.YELLOW);
+		resLabel.setFont(new Font(24.0));
+		resLabel.setStyle("-fx-font-weight:bold;-fx-background-color : #0000FF;");
+
+		// Poner background con metodo setbackground es más tedioso.
+		// resLabel.setBackground(new Background(new BackgroundFill(Color.BLUE,
+		// CornerRadii.EMPTY, Insets.EMPTY)));
+
+		// Permite expandirse horizontalmente
 		HBox.setHgrow(resLabel, Priority.ALWAYS);
 		// Permite expandirse verticalmente.
 		VBox.setVgrow(resLabel, Priority.ALWAYS);
-		
+
 		// Panel contenedor de los anteriores. Deja un espacio de 20 entre
 		// sus nodos hijos y un espacio de 8 respecto a su contendor.
-		VBox root = new VBox(numxPanel,numyPanel,botonesPanel,resLabel);
+		VBox root = new VBox(numxPanel, numyPanel, botonesPanel, resLabel);
 		root.setSpacing(20);
-		
+
 		// Margen interno
-		root.setPadding( new Insets( 8,8,8,8 ) );
-		
+		root.setPadding(new Insets(8, 8, 8, 8));
+
 		// Aplica estilo utiizando css: color y tamaño de borde
-        root.setStyle("-fx-border-color:black; -fx-border-width:2px");
-        
+		root.setStyle("-fx-border-color:black; -fx-border-width:2px");
+
 		// Crea la escena conteniendo el panel vertical
-		Scene scene = new Scene(root,400,250);        
-		
+		Scene scene = new Scene(root, 400, 250);
+
 		// Asinga la escena a la ventana de la aplicación
 		primaryStage.setScene(scene);
-		
+
 		primaryStage.setTitle("Mini Calculadora");
-		
+
 		// Hace que la ventana no se pueda redimensionar.
 		primaryStage.setResizable(true);
-		
+
 		primaryStage.show();
-			
+
 	}
-	
-	private void hazOperacion(char op) {
+
+	private Button creaBotonOp(String op) {
+		Button button = new Button(op);
+		button.setPrefWidth(100);
+		// Anchura máxima infinita
+		button.setMaxWidth(Double.POSITIVE_INFINITY);
+		HBox.setHgrow(button, Priority.ALWAYS);
+		
+		// ** Si queremos que los botones crezcan en altura al crecer su contenedor
+		button.setMaxHeight(Double.POSITIVE_INFINITY);
+		
+		button.setOnAction(e -> hazOperacion(e));
+		return button;
+
+	}
+
+	private void hazOperacion(ActionEvent evento) {
 		int yValor, xValor;
 		try {
-			 xValor = Integer.parseInt(xField.getText());
-			 yValor = Integer.parseInt(yField.getText());
+			xValor = Integer.parseInt(xField.getText());
+			yValor = Integer.parseInt(yField.getText());
 		} catch (NumberFormatException e) {
 			resLabel.setText("x e y deben ser números enteros");
 			return;
 		}
-	
-		int resultado=0;
-		String operString = xField.getText() + " " + op + " " +  yField.getText() + " = ";
+
+		if (!(evento.getSource() instanceof Button))
+			return;
+
+		char op = ((Button) evento.getSource()).getText().charAt(0);
+
+		int resultado = 0;
+		String operString = xField.getText() + " " + op + " " + yField.getText() + " = ";
 		switch (op) {
 		case '+':
 			resultado = xValor + yValor;
@@ -137,7 +141,7 @@ public class CalculadoraSimple extends Application {
 			resultado = xValor * yValor;
 			break;
 		case '/':
-			if(yValor == 0) {
+			if (yValor == 0) {
 				resLabel.setText("Error: División por cero");
 				return;
 			}
@@ -146,9 +150,9 @@ public class CalculadoraSimple extends Application {
 		default:
 			System.err.println("Operación no permitida");
 		}
-		
+
 		resLabel.setText(operString + resultado);
-		
+
 	}
 
 	public static void main(String[] args) {
